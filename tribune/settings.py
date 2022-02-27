7e91b61a59f16dd66650ec6d18779ab2fca61a69
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-28 00:04:40 +0300

    fix deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -80,7 +80,6 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 # Configure for heroku
 
 
-django_heroku.settings(locals())
 
 
 # Application definition
@@ -191,3 +190,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
